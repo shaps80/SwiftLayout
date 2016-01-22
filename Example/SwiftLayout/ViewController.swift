257890c4c9438c29ac5386d9247fb81b7982523a
Shaps Mohsenin <shapsuk@me.com>
2016-01-22 23:33:53 +0000

    Initial commit

---
+++ Example/SwiftLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftLayout
+//
+//  Created by Shaps Mohsenin on 01/22/2016.
+//  Copyright (c) 2016 Shaps Mohsenin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
