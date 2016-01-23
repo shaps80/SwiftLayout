5931849be3d2c51b5d57a50146ab049e60d74cac
Shaps Mohsenin <shapsuk@me.com>
2016-01-23 00:14:34 +0000

    added missing files to repo

---
+++ Example/Pods/Target Support Files/Pods-SwiftLayout_Tests/Pods-SwiftLayout_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftLayout_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftLayout_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-SwiftLayout_Tests/Pods-SwiftLayout_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SwiftLayout_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftLayout_TestsVersionString[];
+
